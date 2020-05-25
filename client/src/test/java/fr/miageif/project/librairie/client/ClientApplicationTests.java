58695223997c7c7fa478d1390e3339462793035d
Dikela <44500887+Dikela@users.noreply.github.com>
2020-05-25 20:16:38 +0200

    implémentation du client

---
+++ client/src/test/java/fr/miageif/project/librairie/client/ClientApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.miageif.project.librairie.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
