93bc33d28e46f21b37695bbab63b189d25324225
Dikela <44500887+Dikela@users.noreply.github.com>
2020-05-27 17:21:43 +0200

    Le client de nos microServices
    implémentation du client UI avec feign, dockerisation des différents microserices

--- client/src/main/java/fr/miageif/project/librairie/client/clientController/ClientController.java
+++ client/src/main/java/fr/miageif/project/librairie/client/clientController/ClientController.java
@@ -1,14 +1,25 @@
 package fr.miageif.project.librairie.client.clientController;
 
+import fr.miageif.project.librairie.client.beans.LivreBean;
+import fr.miageif.project.librairie.client.proxies.ClientProxy;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @Controller
 public class ClientController {
 
+    @Autowired()
+    private ClientProxy clientProxy;
+
     @RequestMapping("/")
     public String acceuil(Model model){
+        List<LivreBean> livres =  clientProxy.listeLivres();
+        model.addAttribute("livres", livres);
+
         return "Accueil";
     }
 }
---
+++ client/src/main/java/fr/miageif/project/librairie/client/proxies/ClientProxy.java
@@ -0,0 +1,20 @@
+package fr.miageif.project.librairie.client.proxies;
+
+import fr.miageif.project.librairie.client.beans.LivreBean;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+
+@FeignClient(name = "microservice-livre", url = "localhost:8003")
+public interface ClientProxy {
+
+    @GetMapping(value ="/Livres")
+    List<LivreBean> listeLivres();
+
+    @GetMapping(value = "/Livres/{isbn}")
+    LivreBean afficherUnLivre(@PathVariable("isbn") String isbn);
+
+}
